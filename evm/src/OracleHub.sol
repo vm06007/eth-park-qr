49d9e4edc6b600a383297cc17bf4893f2985e20a
vonMangoldt <61846574+vonMangoldt@users.noreply.github.com>
2024-11-15 23:13:17 +0700

    create OracleHub shell

---
+++ evm/src/OracleHub.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: BKK
+pragma solidity =0.8.28;
+
+error OnlyOwnerOracleHub(
+    address sender,
+    address owner
+);
+
+contract OracleHub {
+    address owner;
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            OnlyOwnerOracleHub(
+                msg.sender,
+                owner
+            )
+        ),
+        _;
+    }
+
+    function addFeed(
+        address tokenAddress,
+        address oracle
+    )
+        onlyOwner
+        external
+    {
+
+    }
+
+    function getBahtAmount(
+        address tokenAddress,
+        uint256 tokenAmount
+    )
+        external
+        returns (uint256)
+    {
+
+    }
+
+    function getTokenAmount(
+        address tokenAddress,
+        uint256 bahtAmount
+    )
+        external
+        returns (uint256)
+    {
+
+    }
+}
