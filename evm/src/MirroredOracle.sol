6b165906f095a0dec28f80ad44067ab662e09d78
vonMangoldt <61846574+vonMangoldt@users.noreply.github.com>
2024-11-15 23:53:24 +0700

    add function for mirrored oracle

--- evm/src/MirroredOracle.sol
+++ evm/src/MirroredOracle.sol
@@ -12,4 +12,39 @@ contract MirroredOracle {
         _;
     }
 
+    function latestAnswer()
+        external
+        view
+        returns (int256)
+    {
+        return 0;
+    }
+
+    function getRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        return (0, 0, 0, 0, 0);
+    }
+
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        return (0, 0, 0, 0, 0);
+    }
 }
