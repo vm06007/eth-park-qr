d46a34520584c09eb9a326c9593c9c64b704bb37
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2024-11-16 04:35:46 +0700

    route welcome page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 import Footer from "@/components/Footer";
 import { Home } from "@/pages";
+// @ts-ignore
+import Welcome from "@/components/welcome/src/Welcome";
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <main className="min-h-screen flex flex-col justify-between">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/welcome" element={<Welcome />} />
         </Routes>
         <Footer />
         <Toaster />
