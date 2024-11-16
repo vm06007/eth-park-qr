3a374d7a382cd589ab4bfdefb36b97600bb5b467
vonMangoldt <61846574+vonMangoldt@users.noreply.github.com>
2024-11-16 16:20:10 +0700

    import OracleHub

--- evm/script/EthParkQr.s.sol
+++ evm/script/EthParkQr.s.sol
@@ -3,6 +3,7 @@ pragma solidity =0.8.28;
 
 import {Script, console} from "forge-std/Script.sol";
 import {EthParkQr} from "../src/EthParkQr.sol";
+import {OracleHub} from "../src/OracleHub.sol";
 
 contract EthParkQrScript is Script {
     EthParkQr public ethParkQr;
