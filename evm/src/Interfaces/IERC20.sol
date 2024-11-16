f5078176f09c69be1f2dd08e8d53c8db1c26ba87
vonMangoldt <61846574+vonMangoldt@users.noreply.github.com>
2024-11-16 14:28:37 +0700

    Create IERC20.sol

---
+++ evm/src/Interfaces/IERC20.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: BKK
+pragma solidity =0.8.28;
+
+interface IERC20 {
+    function transfer(
+        address to,
+        uint256 value
+    )
+        external
+        returns (bool);
+
+    function balanceOf(
+        address who
+    )
+        external
+        view
+        returns (uint256);
+
+    function allowance(
+        address owner,
+        address spender
+    )
+        external
+        view
+        returns (uint256);
+
+    function approve(
+        address spender,
+        uint256 value
+    )
+        external
+        returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    )
+        external
+        returns (bool);
+}
